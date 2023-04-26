6589c45d936c29a46396d9677472eacf938ed024
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-26 17:15:54 +0900

    Initial commit

---
+++ P20230426/src/p49/CharExample.java
@@ -0,0 +1,15 @@
+package p49;
+
+public class CharExample {
+
+	public static void main(String[] args) {
+		char c1 = 'A';
+		char c2 = 65;
+		int i3 = 65;
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(i3);
+	}
+
+}
