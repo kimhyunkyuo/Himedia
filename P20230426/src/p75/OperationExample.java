a5c65a2d605e5b723f21e8a694bff391c07ecaa8
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-27 15:54:27 +0900

    75번 예제

--- P20230426/src/p75/OperationExample.java
+++ P20230426/src/p75/OperationExample.java
@@ -8,6 +8,7 @@ public class OperationExample {
 		char c1 = 'A' + 1;
 		char c2 = 'A';
 		char c3 = (char)(c2 + 1);
+//		char c4 = c2 + 1;
 		
 		System.out.println("c1 : " + c1);
 		System.out.println("c2 : " + c2);
