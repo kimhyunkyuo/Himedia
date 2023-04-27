8282d74e69c122f5177088afccbc9ff87bec763b
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-27 10:54:50 +0900

    64번 에제 증감++

---
+++ P20230426/src/p64/PPExample.java
@@ -0,0 +1,16 @@
+package p64;
+
+public class PPExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int A = 5;
+		
+		System.out.println(A++);
+		System.out.println(A);
+		
+				
+	}
+
+}
