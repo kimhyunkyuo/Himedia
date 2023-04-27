9af73d861a4944f19ed0925631ebd4917513b00a
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-27 15:57:20 +0900

    68 예제

---
+++ P20230426/src/p68/operatorExample.java
@@ -0,0 +1,18 @@
+package p68;
+
+public class operatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	boolean bPlay = true;
+	
+	System.out.println(bPlay);
+	
+	bPlay = !bPlay;
+	System.out.println(bPlay);
+	
+	bPlay = !bPlay;
+	System.out.println(bPlay);
+	}
+
+}
