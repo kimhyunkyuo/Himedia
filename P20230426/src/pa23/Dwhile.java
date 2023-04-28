8dd285fd8e352723047317f0f93e3a6aa318e43b
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-28 16:59:23 +0900

    do while

---
+++ P20230426/src/pa23/Dwhile.java
@@ -0,0 +1,14 @@
+package pa23;
+
+public class Dwhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i =1;
+		
+		do { System.out.println(i++);
+			
+		}while (i<=5);
+	}
+
+}
