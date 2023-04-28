4a5c2f00f18cc477c24ff49976870d6ebe981d02
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-28 10:42:13 +0900

    83번 예제

---
+++ P20230426/src/p83/Opearation.java
@@ -0,0 +1,28 @@
+package p83;
+
+public class Opearation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str1 = "java";
+		String str2 = "jdk";
+		
+		
+		String str3 = str1 + str2;
+		String str4 = str1 + "" + str2;
+		
+		String str5 = "jdk" + 3 + 3.0;
+		String str6 = 3 + 3.0 + "jdk";
+		
+		
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+		System.out.println(str4);
+		System.out.println(str5);
+		System.out.println(str6);
+		
+	}
+
+}
