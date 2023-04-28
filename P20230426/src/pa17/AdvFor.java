8240f6508a16ca637467919493ead4d46efceef5
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-28 16:46:24 +0900

    증강값

---
+++ P20230426/src/pa17/AdvFor.java
@@ -0,0 +1,23 @@
+package pa17;
+
+public class AdvFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] scores = {9,7,8,9,8};
+		
+		int sum = 0;
+		for(int s : scores) {
+			sum += s;
+		}
+		System.out.println("sum : " + sum);
+		
+		
+		int sum2 = 0;
+		for(int i = 0; i < scores.length; i++) {
+			sum2 += scores[i];
+		}
+		System.out.println("sum2 : " + sum2);
+	}
+
+}
