c15d210272f86892e10f850f3f787e492d920b6b
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-28 16:06:25 +0900

    for반복분

---
+++ P20230426/src/pa14/Repeat2Example.java
@@ -0,0 +1,13 @@
+package pa14;
+
+public class Repeat2Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++) {
+			System.out.println(i);
+			// for([초기화식] ; [조건식] ; [증감식] ) { sysout.out .println() }
+		}
+	}
+
+}
