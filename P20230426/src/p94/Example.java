c6dece47a2f8c964274a4a323adbc14784def74b
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-28 11:03:27 +0900

    94번 제 이항연산자

---
+++ P20230426/src/p94/Example.java
@@ -0,0 +1,14 @@
+package p94;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("45 & 25 = " + (45& 25));
+		System.out.println("45 | 25 = " + (45| 25));
+		System.out.println("45 ^ 25 = " + (45^ 25));
+		System.out.println("~45 = " + (~45));
+	}
+
+}
