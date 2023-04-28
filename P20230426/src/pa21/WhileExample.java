9d07b330cf978e917979fd65a96c40c7873172e8
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-28 16:56:51 +0900

    while문

---
+++ P20230426/src/pa21/WhileExample.java
@@ -0,0 +1,19 @@
+package pa21;
+
+public class WhileExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i =1;
+		while ( i <= 10) {
+			System.out.println(i);
+			i++;
+			
+			
+			if(i==5) {
+				break;
+			}
+		}
+	}
+
+}
