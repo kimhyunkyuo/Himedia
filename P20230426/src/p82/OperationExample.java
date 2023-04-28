b4d4d0759687c1f29722182f3cc2ff27c984175f
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-28 10:30:39 +0900

    82번 예제

---
+++ P20230426/src/p82/OperationExample.java
@@ -0,0 +1,20 @@
+package p82;
+
+public class OperationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 5;
+		double y =0.0;
+		
+		double z = x / y;
+//		 	double z = x % y;
+		 	
+		 	
+		 	System.out.println(Double.isInfinite(z));
+		 	System.out.println(Double.isNaN(z));
+		 	System.out.println( z + 2);
+	}
+
+}
