68f43f8e98586eb5c93aac5d252a55ad2cede413
Class01 <Class01@DESKTOP-P2NCJP3>
2023-04-28 17:03:06 +0900

    break

---
+++ P20230426/src/pa25/Break.java
@@ -0,0 +1,21 @@
+package pa25;
+
+public class Break {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i =1;
+		
+		while (true) {
+			System.out.println(i);
+			
+			
+			if(i == 6)
+				break;
+			
+			i = i + 1;
+		}
+	}
+
+}
